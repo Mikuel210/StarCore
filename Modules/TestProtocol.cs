d7c9239ad594a07b519a000dbc4659bc18b607d0
Miguel <loranvallsmiguel@gmail.com>
2025-10-14 00:51:57 +0200

    Focusing on instances on client

--- Modules/TestProtocol.cs
+++ Modules/TestProtocol.cs
@@ -8,6 +8,19 @@ public class TestProtocol : ProtocolInstance
 	public override void Open()
 	{
 		Output.Info("Hi from test protocol!");
+		
+		new Thread(() => {
+			while (true)
+				ChangeTitle();
+		}).Start();
+	}
+
+	private void ChangeTitle()
+	{
+		Title = "Focus on Starkit";
+		Thread.Sleep(1000);
+		Title = "Focus on RUNTIME";
+		Thread.Sleep(1000);
 	}
 
 }
--- Modules/TestSystem.cs
+++ Modules/TestSystem.cs
@@ -2,7 +2,7 @@
 
 namespace Modules;
 
-[ModuleDescription("A system for testing stuff")]
+[ModuleDescription("A system for testing multiline descriptions. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")]
 public class TestSystem : SystemInstance
 {
 
