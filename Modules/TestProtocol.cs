698954d5731005eac3c52560f58e3a5243caa75d
Miguel <loranvallsmiguel@gmail.com>
2025-10-23 22:27:53 +0200

    Made UI element serialization

--- Modules/TestProtocol.cs
+++ Modules/TestProtocol.cs
@@ -1,4 +1,5 @@
 using SDK;
+using SDK.Communication;
 using SDK.Instances;
 
 namespace Modules;
@@ -11,6 +12,10 @@ public class TestProtocol : ProtocolInstance
 		Output.Info("Hi from test protocol!");
 		
 		InstanceUi.Add(new TextLabel());
+		((TextLabel)InstanceUi[0]).Text = "hiiii";
+		
+		Output.Debug(UiElementData.FromUiElement(InstanceUi[0]));
+		Output.Debug(UiElementData.FromUiElement(InstanceUi[0]).Properties["Text"]);
 	}
 
 }
